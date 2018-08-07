94aabe369e20bd74e61a981e9ba4bfb718b4e8a2
ShotgunEmmet <shotgunemmet@gmail.com>
2018-08-07 22:28:38 +0100

    I re-enabled animations for local client and am working through an engine network tutorial

---
+++ Assets/Engine/Examples/Networking/PlayerSetup.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceneCamera;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            foreach (Behaviour b in componentsToDisable)
+            {
+                b.enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+    
+    void onDisable()
+    {
+        if (sceneCamera)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
