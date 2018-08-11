3958b834b3cdc6234deb171cbc21ded8ffdc16b1
Pierce McKenna <eoinpiercemckenna@gmail.com>
2018-08-11 15:44:49 +0100

    added multiplayer lobby to test networking

---
+++ Assets/Engine/Examples/Networking/PlayerMovementController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerMovementController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 2.4f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        Vector2 move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed;
+
+        motor.Move(move);
+    }
+
+}
