52185f92d44bc133b7f3e4a006ba30acf6edfc1a
ShotgunEmmet <shotgunemmet@gmail.com>
2018-08-15 22:36:36 +0100

    Networked Footprints (well not really)

---
+++ Assets/Engine/Examples/Networking/Footprint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footprint : MonoBehaviour {
+
+    private void Start()
+    {
+        GetComponent<Animator>().Play("Footprints BlendTree");
+    }
+
+    void DeleteOnEnd()
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
